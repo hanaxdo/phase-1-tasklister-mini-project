3824b00ab4ea584d0ff180205ca8b200870ffaae
Hana Do <hanaxdo@gmail.com>
2021-04-17 21:12:48 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const task = document.getElementById("create-task-form")
+  const list = document.getElementById("tasks")
+  
+  task.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    
+    document.createElement(`list`)
+    
+    const addElement = () => {
+
+      const capturesText = e.target[0].value
+      const grabLi = document.createElement("li");
+      const createLi = grabLi.innerHTML
+
+      list.appendChild(grabLi)
+      
+      grabLi.innerHTML = capturesText
+
+      return createLi
+    }   
+    return addElement()
+  })
+  
 });
